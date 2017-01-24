6a53a81163f99457f12d0f0581a619468235fbc0
Austin Kabiru <austin.kabiru@andela.com>
2017-01-24 12:33:00 +0300

    standardize webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,7 @@ module.exports = {
     loaders: [
       {
         test: /\.(css|scss)$/,
-        loaders: [
-          'style-loader',
-          'css-loader',
-        ]
+        loader : 'style!css'
       },
       {
         test:    /\.html$/,
@@ -34,11 +31,11 @@ module.exports = {
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        loader: 'url?limit=10000&mimetype=application/font-woff',
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
+        loader: 'file',
       },
     ],
 
