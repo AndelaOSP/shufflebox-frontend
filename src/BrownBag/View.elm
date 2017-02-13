6736db77804e82920cc0864f207c70ba0fb8d889
Penina Wanjiru <penina.wanjiru@andela.com>
2017-02-13 12:23:28 +0300

    WIP

--- src/BrownBag/View.elm
+++ src/BrownBag/View.elm
@@ -9,20 +9,43 @@ import BrownBag.Models exposing (BrownBagPresenter, BrownBagStatus(..))
 view : List BrownBagPresenter -> Html Msg
 view brownBags =
     div [ class "content-container" ]
-        [ div [ class "sidebar" ]   
-            [ text "sidebar" ]
-        , div [ class "all-content" ]
-            [ text "all" ]
+        [ div [ class "sidebar" ] [ sideNav]
+        , div [ class "all-content" ][ navBar ]
         ]
 
+ 
+navBar : Html Msg
+navBar =
+    nav [ class "nav-bar" ]
+        [ div [ class "list"] [ ul []
+            [ li [][ a [href "#brownbags"][text "Brown Bag"] ]
+            , li [][ a [href "#secretsanta"][text "Secret Santa" ] ]
+            , li [][ a [href "#hangouts"][text "Hangouts" ] ]
+            ]]
+        , div [ class "image"] [img
+            [ src "https://www.dropbox.com/s/okgmtdpih1xxau3/Shuffle.png?raw=1"
+            , width 50
+            ]  []]
+        ]
+sideNav =  
+        div [ class "sidenav" ]
+            [ nav [][text "solo"]
+            ]
 
--- feed : List BrownBagPresenter -> Html Msg
--- feed brownBags =
---     div []
---         [ previous brownBags
---         , upcoming brownBags
---           -- , next brownBags
---         ]
+
+
+    
+
+
+
+
+--feed : List BrownBagPresenter -> Html Msg
+--feed brownBags =
+-- div []
+--     [ previous brownBags
+--     , upcoming brownBags
+--       -- , next brownBags
+--     ]
 
 
 -- previous : List BrownBagPresenter -> Html msg
