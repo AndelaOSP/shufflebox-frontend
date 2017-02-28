7c29d7fdd331340afc289e5cdfa3b5c279456067
Penina Wanjiru <penina.wanjiru@andela.com>
2017-02-28 15:40:41 +0300

    add button to hangout page

--- src/App/Hangouts/View.elm
+++ src/App/Hangouts/View.elm
@@ -10,14 +10,114 @@ view =
         [ table []
             [ tr [] [ th [] [ text "HANGOUTS" ] ]
             , tr []
-                [ text "James"
-                , img
+                [ img
                     [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
                     , width 50
                     ]
                     []
+                , text "James Kinuthia"
                 ]
-            , tr [] [ text "table" ]
-            , tr [] [ text "table" ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            , tr []
+                [ img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                , text "James Kinuthia"
+                ]
+            ]
+        , div [ class "hangout--button" ]
+            [ button []
+                [ img [ src "https://www.dropbox.com/s/okgmtdpih1xxau3/Shuffle.png?raw=1" ] []
+                , b [] [ text "SHUFFLE" ]
+                ]
             ]
         ]
