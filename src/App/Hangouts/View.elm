18338f717c1e70dc937df9517897472d7552dcb3
Penina Wanjiru <penina.wanjiru@andela.com>
2017-02-28 12:44:51 +0300

    change table data

--- src/App/Hangouts/View.elm
+++ src/App/Hangouts/View.elm
@@ -9,7 +9,14 @@ view =
     div [ class "table" ]
         [ table []
             [ tr [] [ th [] [ text "HANGOUTS" ] ]
-            , tr [] [ text "table" ]
+            , tr []
+                [ text "James"
+                , img
+                    [ src "https://api.adorable.io/avatars/285/abott@adorable.io.png"
+                    , width 50
+                    ]
+                    []
+                ]
             , tr [] [ text "table" ]
             , tr [] [ text "table" ]
             ]
