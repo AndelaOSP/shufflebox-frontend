e2249424d69ca3e39e287d7c4887926fce01039a
Penina Wanjiru <penina.wanjiru@andela.com>
2017-03-03 16:44:16 +0300

    Create hangouts view 140313371 (#21)
    * add table to hangouts page
    * change table data
    * add button to hangout page
    * add button to hangout page
    * visualize hangouts
    * add colspan

--- src/App/Hangouts/View.elm
+++ src/App/Hangouts/View.elm
@@ -1,8 +1,47 @@
 module App.Hangouts.View exposing (view)
 
-import Html exposing (Html, span, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import App.Hangouts.Models exposing (Hangout, Group, Member)
 
 
-view : Html msg
-view =
-    span [] [ text "Hangouts is awesome!!" ]
+view : List Hangout -> Html msg
+view hangouts =
+    div [ class "table" ]
+        [ table []
+            (tr [] [ th [ colspan 4 ] [ text "HANGOUTS" ] ] :: hangoutsView hangouts)
+        , div [ class "hangout--button" ]
+            [ button []
+                [ img [ src "https://www.dropbox.com/s/okgmtdpih1xxau3/Shuffle.png?raw=1" ] []
+                , b [] [ text "SHUFFLE" ]
+                ]
+            ]
+        ]
+
+
+hangoutsView : List Hangout -> List (Html msg)
+hangoutsView hangouts =
+    List.concatMap hangoutView hangouts
+
+
+hangoutView : Hangout -> List (Html msg)
+hangoutView hangout =
+    List.map groupView hangout.groups
+
+
+groupView : Group -> Html msg
+groupView group_ =
+    tr []
+        (List.map memberView group_.members)
+
+
+memberView : Member -> Html msg
+memberView member =
+    td []
+        [ img
+            [ src member.avatar
+            , width 50
+            ]
+            []
+        , text member.name
+        ]
