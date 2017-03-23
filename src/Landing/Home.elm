a2035b280de58274a45092e9e9ba0bfd3ce9ebb3
Penina Wanjiru <penina.wanjiru@andela.com>
2017-03-23 21:00:44 +0300

    set up bulma

--- src/Landing/Home.elm
+++ src/Landing/Home.elm
@@ -13,10 +13,9 @@ view =
         , illustration
         ]
 
-
 introSection : Html Msg
 introSection =
-    div [ class "intro" ]
+    div [ class "intro column is-one-third is-paddingless" ]
         [ img
             [ src "https://www.dropbox.com/s/okgmtdpih1xxau3/Shuffle.png?raw=1"
             , width 500
@@ -43,10 +42,13 @@ introSection =
 
 illustration : Html msg
 illustration =
-    div [ class "illustration" ]
-        [ img
-            [ src "https://www.dropbox.com/s/l6wx0xixdfeilso/final.png?raw=1"
-            , width 600
+    div [ class "illustration column is-two-thirds is-paddingless" ]
+        [ div 
+            [ class "image is-4by3"]
+            [ img
+                [ src "https://www.dropbox.com/s/l6wx0xixdfeilso/final.png?raw=1"
+                --, width 600
+                ]
+                []
             ]
-            []
         ]
