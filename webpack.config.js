820d2bee89187908ad1e3e370d64e9ea75539fe9
zacck <coderv63@gmail.com>
2017-04-06 14:32:41 +0200

    refresh gitignore to get dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,83 @@
 const path = require("path");
 const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+
+
+
+//enviroment variable
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
+const PRODUCTION = process.env.NODE_ENV === 'production';
+const TEST = process.env.NODE_ENV === 'test'
+
+const entry = PRODUCTION
+      ?     {
+              app:
+              [
+                '/index.js'
+              ],
+              vendor:
+              [
+                'bulma',
+              ]
+            }
+      :
+              [
+                './index.js'
+              ];
+
+const plugins = PRODUCTION
+      ?     [
+        new webpack.optimize.CommonsChunkPlugin({name:'vendor', filename:'vendor.[hash:12].min.js'}),
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            warnings: false,
+            screw_ie8: true,
+            dead_code: true,
+            unused: true
+          }
+        }),
+        new HTMLWebpackPlugin({
+          template:'index-template.html'
+        }),
+        new CompressionPlugin({
+          asset: "[path].gz[query]",
+          algorithm: "gzip",
+          test: /\.js$|\.css$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.8
+        }),
+        new SWPrecacheWebpackPlugin(
+          {
+            cacheId: 'hapihour',
+            filename: 'hapi-sw.js',
+            forceDelete: true,
+            minify: true,
+            skipWaiting: true,
+            verbose: true
+          }
+        )
+      ]
+    : [
+        new webpack.NamedModulesPlugin()
+      ];
+
+//define plugins for both envs
+plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': { NODE_ENV: JSON.stringify('production') }
+  })
+);
 
 const config = {
-  entry: {
-    app: [
-      './src/index.js'
-    ]
-  },
-
+  context: __dirname + '/src',
+  entry: entry,
   output: {
-    path: path.resolve(__dirname + '/dist'),
-    filename: '[name].js',
-    publicPath: '/'
+    path: PRODUCTION ?  '/' : '/',
+    filename: PRODUCTION ? '[name].[hash:12].min.js' : '[name].js',
+    publicPath: __dirname + 'dist'
   },
 
   module: {
@@ -48,39 +114,15 @@ const config = {
 
     noParse: /\.elm$/,
   },
-
+  plugins:plugins,
   devServer: {
-    stats: {
-      colors: true,
-      chunks: false
-    }
-  }
-};
-
-if (process.env.NODE_ENV !== 'production') {
-  config.entry.app = [
-    'webpack-dev-server/client?http://localhost:3000',
-    ...config.entry.app
-  ];
-} else {
-  config.plugins = [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true,
-        warnings: false
-      },
-      mangle: {
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-        screw_ie8: true
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify('production') }
-    })
-  ];
+    contentBase: './dist',
+   inline: true,
+   hot: true
+ },
+ devtool: 'source-map'
 }
+
+
 
 module.exports = config;
